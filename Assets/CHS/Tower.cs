d2ae5f96be6c2153d08aac7f4498e5c829a580cc
혜성 <nalbajo@naver.com>
2025-04-28 17:26:35 +0900

    [feat] 타워 기본 요소 추가
    상속될 타워의 기본 요소를 작성한다

--- Assets/CHS/Tower.cs
+++ Assets/CHS/Tower.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public interface ITower
 {
-    
+    public float Damage { get; }
+    public float Range { get; }
+    public float AttackSpeed { get; }
+    void Attack(); // target: enemy
+    void Upgrade();
 }

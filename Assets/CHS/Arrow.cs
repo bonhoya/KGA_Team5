be936b44ace19f49c33599b63677f215d9e6e428
혜성 <nalbajo@naver.com>
2025-04-28 18:16:42 +0900

    [feat] 화살이 앞으로 날아가고 적의 체력을 깎도록 추가
    화살이 생성될 때마다 앞으로 나가고 적을 만나면 적의 체력을 깎도록 한다

--- Assets/CHS/Arrow.cs
+++ Assets/CHS/Arrow.cs
@@ -5,5 +5,24 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
 
-    
+    [SerializeField] private Rigidbody rigid;
+
+    [SerializeField] private float arrowSpeed;
+    [SerializeField] private int enemyHealth;
+    [SerializeField] private int arrowPower;
+
+    public Stack<GameObject> returnPool;
+
+    private void OnEnable()
+    {
+        rigid.AddForce(transform.forward * arrowSpeed, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            enemyHealth -= arrowPower;
+        }
+    }
 }

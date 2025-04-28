21c9a69fc2ffd34965de1428a7947b75356bf9a5
bonhoya <imhoya0407@naver.com>
2025-04-28 14:15:21 +0900

    [feat] 게임 사운드매니저 구현
    게임에서 사운드매니저를 통해 플레이어가 소리를 BGM의 소리를 조절할 수 있도록 구현했다.

--- Assets/LMH/Scripts/SoundsManager.cs
+++ Assets/LMH/Scripts/SoundsManager.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Accessibility;
+// 사운드를 사용할 네임스페이스 선언 구문
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SoundsManager : MonoBehaviour
 {
     public static SoundsManager Instance;
-
-    
+    public AudioSource bgm;
+    public AudioClip[] bgmlist;
+    public AudioMixer audioMixer;
 
     private void Awake()
     {
@@ -15,6 +21,8 @@ public class SoundsManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            //SceneManager.sceneLoaded += OnSceneLoaded;
+            backGroundMusicPlay(bgmlist[0]);
         }
         else
         {
@@ -22,8 +30,36 @@ public class SoundsManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        
+    }
+
     private void OnEnable()
     {
         
+    }
+    // 기본적으로 출력하는 배경음악
+    public void backGroundMusicPlay(AudioClip cilp)
+    {
+        bgm.outputAudioMixerGroup = audioMixer.FindMatchingGroups("BGM")[0];
+        bgm.clip = cilp;
+        bgm.loop = true;
+        bgm.volume = 0.5f;
+        bgm.Play();
+    }
+    // 씬에 의해 변경되는 배경음악
+    /*private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for(int i = 0; i < bgmlist.Length; i++)
+        {
+            if (arg0.name == bgmlist[i].name);
+        }
+    }*/
+
+    // UI와 연결되는 배경음악 컨트롤러
+    public void BGMController(float value)
+    {
+        audioMixer.SetFloat("BGMParam", Mathf.Log10(value) * 20);
     }
 }

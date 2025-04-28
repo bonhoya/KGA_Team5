da51c6ac2aa094f6a300634881b7f188ef1f9b1d
bonhoya <imhoya0407@naver.com>
2025-04-28 16:17:40 +0900

    [feat] 씬 전환 매니저 구현
    씬 전환할때 사용할 매니저를 구현했다.

--- Assets/LMH/Scripts/SceneChangeManager.cs
+++ Assets/LMH/Scripts/SceneChangeManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static SceneChangeManager Instance;
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }

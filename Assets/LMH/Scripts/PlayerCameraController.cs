d90a5928bb39ed271e8c2db77ffc7c55a71e9401
bonhoya <imhoya0407@naver.com>
2025-04-29 17:12:03 +0900

    [feat] 카메라 움직임에 따른 컨트롤 구현
    플레이어가 카메라를 이용하여 타깃을 선택하거나 움직이고 줌 인, 줌 아웃을 할 수 있도록 구현했다.

---
+++ Assets/LMH/Scripts/PlayerCameraController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [Header("Camera Speed Adjust")]
+    [SerializeField] private Camera mainCamera;
+
+    [Header("Camera Speed Adjust")]
+    [SerializeField] private float cameraMoveSpeed;
+    [SerializeField] private float cameraMouseMoveSpeed;
+    [SerializeField] private float cameraMouseZoomSpeed;
+
+    [Header("ZoomLimit")]
+    [SerializeField] private float cameraZoomInLimit;
+    [SerializeField] private float cameraZoomOutLimit;
+    private Vector3 inputKeyPos;
+    private Vector2 inputMousePos;
+    private Vector3 hitRayPos;
+    private float mouseScroll;
+
+    private int layerNum;
+    private string hitTarget;
+
+    private void Update()
+    {
+        KeyMove();
+        MouseMove();
+        MouseZoomInOut();
+        DetectCamera();
+    }
+
+    private void KeyMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        inputKeyPos = new Vector3(x, 0, z).normalized;
+
+        transform.Translate(inputKeyPos * cameraMoveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void MouseMove()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            inputMousePos = Input.mousePosition;
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            Vector2 preMousePos = (Vector2)Input.mousePosition - inputMousePos;
+            Vector3 isMovedPos = new Vector3(preMousePos.x, 0, preMousePos.y);
+            transform.Translate(isMovedPos * cameraMouseMoveSpeed * Time.deltaTime, Space.World);
+
+            preMousePos = Input.mousePosition;
+        }
+    }
+
+    private void MouseZoomInOut()
+    {
+        mouseScroll = Input.mouseScrollDelta.y;
+        mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView - mouseScroll, cameraZoomInLimit, cameraZoomOutLimit);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, hitRayPos);
+    }
+
+    private void DetectCamera()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                hitRayPos = hitInfo.point;
+                layerNum = hitInfo.collider.gameObject.layer;
+                hitTarget = LayerMask.LayerToName(layerNum);
+                Debug.Log($"현재 찍힌 위치 {hitRayPos}"); // 위치 확인용 디버그
+                Debug.Log($"현재 찍힌 대상 {hitTarget}"); // 대상 확인용 디버그
+
+            }
+        }
+    }
+}

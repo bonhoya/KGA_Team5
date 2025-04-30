df021493e62cccdc80678ca03c16df90a658dc69
HugeChamChi <abaebae96@gmail.com>
2025-04-30 11:57:02 +0900

    [Feat]Ghost 몬스터 구현
    - 마법저항력 20%
    - 물리공격력은 통하지 않도록 구현

--- Assets/USW/Script/MonsterScript/Ghost.cs
+++ Assets/USW/Script/MonsterScript/Ghost.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
+public class Ghost : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public override void InitializeStats()
     {
+        maxHealth = 50f; 
+        currentHealth = maxHealth; 
+        defense = 1f; // 물리 방어력 100% 
+        magicResistance = 0.3f; // 마법 저항력 30% 
+        moveSpeed = 6f; 
         
+        // NavMeshAgent 속도 업데이트
+        if (agent != null)
+        {
+            agent.speed = moveSpeed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 피지컬 대미지를 받았을 때 피해를 입지 않도록 재정의
+    public override void TakePhysicalDamage(float attackerAttack)
     {
+      
+        float damage = GameManager.Instance.CalculatePhysicalDamage(attackerAttack, this.defense);
         
+        currentHealth -= damage; 
+        if (currentHealth <= 0) Die(); // 만약의 경우를 대비해 체크
+    }
+
+    // 매직 대미지는 피해를 받도록 재정의
+    public override void TakeMagicDamage(float attackerMagic)
+    {
+        float damage = GameManager.Instance.CalculateMagicDamage(attackerMagic, this.magicResistance);
+        currentHealth -= damage;
+        if (currentHealth <= 0) Die();
     }
 }

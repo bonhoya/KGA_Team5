832dd48fc45a41fc402f759646a923c0540bda2c
bonhoya <imhoya0407@naver.com>
2025-05-02 12:07:32 +0900

    [feat] UI매니저의 기능을 구현
    UI매니저는 다음과 같은 기능을 가진다.
    1. Dic에 UI들을 보관한다.
    2. 담겨진 UI들을 사용한다.
    3. 해당 UI들을 활성화하거나 비활성화 한다.

--- Assets/LMH/Scripts/UIManager.cs
+++ Assets/LMH/Scripts/UIManager.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance { get; private set; }
+
+    private Dictionary<string, GameObject> uiDic = new Dictionary<string, GameObject>();
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        GameObject mainScreenUI = GameObject.Find("MainMenuCanvas");
+        SetUI("MainScreenUI", mainScreenUI);
+        Debug.Log("현재 메인 스크린 UI가 저장되었습니다.");
+    }
+
+    /// <summary>
+    /// UI를 Dictionary에 저장
+    /// </summary>
+    /// <param name="key">UI에 저장될 UI이름</param>
+    /// <param name="uiObject">저장하고자 하는 대상</param>
+    public void SetUI(string key, GameObject uiObject)
+    {
+        if (uiDic.ContainsKey(key) == false)
+        {
+            uiDic.Add(key, uiObject);
+        }
+        else
+            Debug.Log($"UIManager: 현재 {key}는 이미 등록된 키 입니다."); //
+    }
+
+    public GameObject GetUI(string key)
+    {
+        if (uiDic.TryGetValue(key, out GameObject uiObjcet))
+        {
+            return uiObjcet;
+        }
+        else
+            return null;
+    }
+
+    /// <summary>
+    /// UI를 보이게(활성화) 하기
+    /// </summary>
+    /// <param name="key">UI에 저장될 UI이름</param>
+    public void ShowUI(string key)
+    {
+        GameObject uiObject = GetUI(key);
+        if(uiObject != null)
+        {
+            uiObject.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// UI를 안보이게(비활성화) 하기
+    /// </summary>
+    /// <param name="key">UI에 저장될 UI이름</param>
+    public void HideUI(string key)
+    {
+        GameObject uiObject = GetUI(key);
+        if(uiObject != null)
+        {
+            uiObject.SetActive(false);
+        }
     }
 }
